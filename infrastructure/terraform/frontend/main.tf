3dc2b54e6e092491744028b13f695ddcea661798
meezee91 <muhammadtarmizisulaiman@gmail.com>
2024-06-10 21:22:41 +0800

    storing frontend files to S3 bucket and allowing public access

--- infrastructure/terraform/frontend/main.tf
+++ infrastructure/terraform/frontend/main.tf
@@ -43,3 +43,38 @@ resource "aws_iam_role_policy" "s3_bucket_policy" {
   })
 }
 
+# S3 Bucket to store the frontend files
+resource "aws_s3_bucket" "frontend" {
+  bucket = "group-3-frontend-${var.branch_name}"
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+# Uploading frontend build files to S3 bucket
+resource "aws_s3_object" "frontend" {
+  for_each = fileset("frontend/build", "**/*")
+
+  bucket = aws_s3_bucket.frontend.bucket
+  key    = each.value
+  source = "frontend/build/${each.value}"
+}
+
+# Policy to allow public read access to the S3 bucket
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.frontend.bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid: "PublicReadGetObject",
+        Effect = "Allow",
+        Principal: "*",
+        Action: "s3:GetObject",
+        Resource: "arn:aws:s3:::${aws_s3_bucket.frontend.bucket}/*"
+      }
+    ]
+  })
+}
