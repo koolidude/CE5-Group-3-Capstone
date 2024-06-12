cd0e5cfa7a1163e94b436cac6f2e8d9cff256b91
Hnin <hwutyee@gmail.com>
2024-06-12 23:24:51 +0800

    updating app.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,15 +1,26 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
 // Mock the fetch function to return sample data for the movies endpoint
 beforeEach(() => {
-  global.fetch = jest.fn(() =>
-    Promise.resolve({
-      json: () => Promise.resolve({ results: [{ id: 1, title: 'Movie Title', poster_path: '/path/to/poster.jpg' }] }),
-    })
-  );
+  process.env.REACT_APP_BACKEND_URL = 'https://mock-backend-url.com:5000';
+
+  global.fetch = jest.fn((url) => {
+    if (url === `${process.env.REACT_APP_BACKEND_URL}/movies`) {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({ results: [{ id: 1, title: 'Movie Title', poster_path: '/path/to/poster.jpg' }] }),
+      });
+    } else {
+      return Promise.reject(new Error('Unknown URL'));
+    }
+  });
+});
+
+afterEach(() => {
+  jest.resetAllMocks();
 });
 
 test('renders Netflix Clone header', () => {
@@ -20,6 +31,7 @@ test('renders Netflix Clone header', () => {
 
 test('fetches and displays movies', async () => {
   render(<App />);
-  const movieElement = await screen.findByText(/Movie Title/i);
+  const movieElement = await waitFor(() => screen.getByText(/Movie Title/i));
   expect(movieElement).toBeInTheDocument();
 });
+
